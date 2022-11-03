ee06c02cd4d0efcd6521a90db1c92e3a6f43c3eb
spiderforrest <spider@spood.org>
2022-11-03 11:54:35 -0700

    added test three

--- test/tests.js
+++ test/tests.js
@@ -41,11 +41,26 @@ test('this is the second test', (expect) => {
     const expected = 28;
     const actual = multiplyBySeven(4);
     expect.equal(actual, expected);
-
     const expected2 = 21;
     const actual2 = multiplyBySeven(3);
     expect.equal(actual2, expected2);
     const expected3 = 31633;
     const actual3 = multiplyBySeven(4519);
+    expect.equal(actual3, expected3);
+});
+
+// import function
+import { multiplyBy12ThenHalve } from '../functions.js';
+
+// run test
+test('this is the second test', (expect) => {
+    const expected = 10;
+    const actual = multiplyBy12ThenHalve(8, 4, 5);
+    expect.equal(actual, expected);
+    const expected2 = 12;
+    const actual2 = multiplyBy12ThenHalve(10, 5, 6);
+    expect.equal(actual2, expected2);
+    const expected3 = 672;
+    const actual3 = multiplyBy12ThenHalve(7872, 82, 7);
     expect.equal(actual3, expected3);
 });
