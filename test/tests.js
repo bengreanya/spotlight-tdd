0c351282cbd03da83b9068e842ce41e89ce61fc0
spiderforrest <spider@spood.org>
2022-11-03 12:17:03 -0700

    added test for array and fixed formatting

--- test/tests.js
+++ test/tests.js
@@ -42,9 +42,11 @@ test('mult7test', (expect) => {
     const expected = 28;
     const actual = multiplyBySeven(4);
     expect.equal(actual, expected);
+
     const expected2 = 21;
     const actual2 = multiplyBySeven(3);
     expect.equal(actual2, expected2);
+
     const expected3 = 31633;
     const actual3 = multiplyBySeven(4519);
     expect.equal(actual3, expected3);
@@ -54,9 +56,11 @@ test('multiplyBy12ThenHalve', (expect) => {
     const expected = 24;
     const actual = multiplyBy12ThenHalve(4);
     expect.equal(actual, expected);
+
     const expected2 = 48;
     const actual2 = multiplyBy12ThenHalve(8);
     expect.equal(actual2, expected2);
+
     const expected3 = 96;
     const actual3 = multiplyBy12ThenHalve(16);
     expect.equal(actual3, expected3);
@@ -70,10 +74,28 @@ test('divideThenMultTest', (expect) => {
     const expected = 10;
     const actual = divideThenMultiply(8, 4, 5);
     expect.equal(actual, expected);
+
     const expected2 = 12;
     const actual2 = divideThenMultiply(10, 5, 6);
     expect.equal(actual2, expected2);
+
     const expected3 = 672;
     const actual3 = divideThenMultiply(7872, 82, 7);
     expect.equal(actual3, expected3);
+});
+
+import { returnAsAnArray } from '../functions.js';
+
+test('return as array test', (expect) => {
+    const expected = [8, 4, 5];
+    const actual = returnAsAnArray(8, 4, 5);
+    expect.deepEqual(expected, actual);
+
+    const expected2 = [3, 8, 22];
+    const actual2 = returnAsAnArray(3, 8, 22);
+    expect.deepEqual(expected2, actual2);
+
+    const expected3 = [-2, 4, 2000];
+    const actual3 = returnAsAnArray(-2, 4, 2000);
+    expect.deepEqual(expected3, actual3);
 });
